a74938729a3353ce813e0e773fd64a26dd07e8ec
Jon Kirkman <jonkirkman@gmail.com>
2013-11-17 23:49:21 -0700

    added lat & lon fields to the Location class

--- lib/src/geographic/location.dart
+++ lib/src/geographic/location.dart
@@ -4,6 +4,14 @@ part of mapbits;
 /**
  * A geographic location, expressed by latitude and longitude.
  */
-class Location {
-  Location();
+class Location extends Point {
+
+  Location(num lat, num lon) : super(lon, lat);
+
+  // Alias lat (latitude) to y
+  num get lat          => y;
+      set lat(num val) => y = val;
+  // Alias lon (longitude) to x
+  num get lon          => x;
+      set lon(num val) => x = val;
 }
